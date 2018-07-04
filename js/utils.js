d41e77a8413b351b59b8ce76cf172d4e9571ad46
SashaKa <katushkovasa@gmail.com>
2018-07-04 13:03:44 +0300

    added error msg

--- js/utils.js
+++ js/utils.js
@@ -20,11 +20,11 @@
     },
     createErrorMessage: function (errorMessage) {
       var node = document.createElement('div');
-      node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+      node.style = 'z-index: 10; margin: 2% auto; text-align: center; background-color: tomato;';
       node.style.position = 'absolute';
       node.style.left = 0;
       node.style.right = 0;
-      node.style.fontSize = '30px';
+      node.style.fontSize = '24px';
       node.textContent = errorMessage;
       document.body.insertAdjacentElement('afterbegin', node);
     }
