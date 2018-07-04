8a1a18c8447f527f3b7d07656721559609c85f1f
SashaKa <katushkovasa@gmail.com>
2018-07-04 12:01:47 +0300

    module4-task1

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,6 @@
 'use strict';
 
 (function () {
-  var MIN_COMMENTS = 1;
-  var MAX_COMMENTS = 15;
-  var MIN_LIKES = 15;
-  var MAX_LIKES = 200;
   var MAX_POSTS = 25;
   var ESC_KEY = 27;
   var postTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
@@ -13,34 +9,34 @@
   var commentsContainer = document.querySelector('.social__comments');
   var socialCommentCounter = document.querySelector('.social__comment-count');
   var socialLoadMore = document.querySelector('.social__loadmore');
+  var bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
 
-  // Created the post
+  // Hendlers
 
-  var generateComment = function () {
-    var postComment = [];
-    var randomTimes = window.utils.getRandomNumber(MIN_COMMENTS, MAX_COMMENTS);
-    for (var i = 0; i < randomTimes; i++) {
-      postComment.push(window.data.sentences[window.utils.getRandomNumber(0, window.data.sentences.length - 1)]);
-    }
-    return postComment;
+  var openBigPictureHandler = function (evt) {
+    evt.preventDefault();
+    renderBigPicture(evt);
+    document.addEventListener('keydown', escBigPictureHandler);
   };
 
-  // Created array of posts
+  var closeBigPictureHandler = function () {
+    window.utils.addHidden(bigPicture);
+    bigPictureClose.removeEventListener('keydown', escBigPictureHandler);
+    window.uploadPicture.innerHtml = '';
+  };
+
+  var escBigPictureHandler = function (evt) {
+    if (evt.keyCode === ESC_KEY) {
+      window.utils.addHidden(bigPicture);
+    }
+  };
+
+  // Posts Arrays
 
   var posts = [];
-  var generateAllPosts = function () {
-    for (var i = 0; i < MAX_POSTS; i++) {
-      posts[i] = {
-        url: 'photos/' + (i + 1) + '.jpg',
-        likes: window.utils.getRandomNumber(MIN_LIKES, MAX_LIKES),
-        comments: generateComment(),
-        description: window.utils.getRandomFromArr(window.data.descriptions)
-      };
-    }
-  };
-    // Showed pictures
+  var successHandler = function (response) {
+    posts = response;
 
-  var renderPictures = function () {
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < MAX_POSTS; i++) {
       var postElement = postTemplate.cloneNode(true);
@@ -54,9 +50,13 @@
     }
 
     similarListElement.appendChild(fragment);
+    var picturePreview = document.querySelectorAll('.picture__link');
+    for (var j = 0; j < picturePreview.length; j++) {
+      picturePreview[j].addEventListener('click', openBigPictureHandler);
+    }
   };
 
-    // Rendered Big Picture
+  // Big Picture Rendering
 
   var renderBigPicture = function (evt) {
     window.utils.removeHidden(bigPicture);
@@ -64,16 +64,16 @@
     bigPicture.querySelector('.big-picture__img img').src = evt.target.src;
     bigPicture.querySelector('.likes-count').textContent = posts[pictureIndex].likes;
     bigPicture.querySelector('.comments-count').textContent = posts[pictureIndex].comments.length;
-    bigPicture.querySelector('.social__caption').textContent = posts[pictureIndex].description;
+    bigPicture.querySelector('.social__caption').textContent = posts[pictureIndex].comments[0];
 
-    // Removed old comments
+    // Delete old comments
 
     var commentsToRemove = commentsContainer.querySelectorAll('.social__comment');
     commentsToRemove.forEach(function (comment) {
       commentsContainer.removeChild(comment);
     });
 
-    // Rendered comments
+    // Rendering new comments
 
     var maxRenderedComments;
     if (posts[pictureIndex].comments.length > 5) {
@@ -100,38 +100,13 @@
       newComment.appendChild(newCommentText);
     }
   };
-
-  generateAllPosts();
-  renderPictures();
   window.utils.addVisuallyHidden(socialCommentCounter);
   window.utils.addVisuallyHidden(socialLoadMore);
 
-  // Showed Big Picture on the click mini picture
-
-  var picturePreview = document.querySelectorAll('.picture__link');
-  var bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
-
-  var openBigPictureHandler = function (evt) {
-    evt.preventDefault();
-    renderBigPicture(evt);
-    document.addEventListener('keydown', escBigPictureHandler);
-  };
-
-  var closeBigPictureHandler = function () {
-    window.utils.addHidden(bigPicture);
-    bigPictureClose.removeEventListener('keydown', escBigPictureHandler);
-    window.uploadPicture.innerHtml = '';
-  };
-
-  var escBigPictureHandler = function (evt) {
-    if (evt.keyCode === ESC_KEY) {
-      window.utils.addHidden(bigPicture);
-    }
-  };
-
-  for (var i = 0; i < picturePreview.length; i++) {
-    picturePreview[i].addEventListener('click', openBigPictureHandler);
-  }
   bigPictureClose.addEventListener('click', closeBigPictureHandler);
-})();
 
+  var errorHandler = function (errorMessage) {
+    window.utils.createErrorMessage(errorMessage);
+  };
+  window.backend.download(successHandler, errorHandler);
+})();
